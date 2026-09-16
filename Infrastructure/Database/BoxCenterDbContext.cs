ec33bdfc58f6090a1475d4dda5c956decda602ff
Javier Carchipulla Sisalima <krchipullajavier@gmail.com>
2026-09-15 21:22:24 -0500

    Agregar archivos de proyecto.

---
+++ Infrastructure/Database/BoxCenterDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api_BoxCenter.Infrastructure.Database
+{
+    public class BoxCenterDbContext: DbContext
+    {
+        // implementamos DbContext
+        public BoxCenterDbContext(DbContextOptions<BoxCenterDbContext> options): base(options) {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Scalar.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    // implementar scalar
+    app.MapScalarApiReference();
+}
+
+//app.MapGet("/", () => "hola que tal, tu como estas.");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
